54de717fa33a8546c242b6e3033c44ad5688a838
JMNSY <moses1991@gmail.com>
2015-01-18 20:56:15 +0800

    Update KTXP&dmhyTorrentLinkToMagnet.user.js
    发现动漫花园进行了一次页面升级，目前花园的资源列表中已经存在磁链，本次脚本升级主要是将动漫花园的资源列表页从作用域中移除
    如果当极影回归之后发现搜索页中也自带磁链的话，那么我将不会继续更新该脚本，直到我再次脑洞大开

--- KTXP&dmhyTorrentLinkToMagnet.user.js
+++ KTXP&dmhyTorrentLinkToMagnet.user.js
@@ -1,11 +1,9 @@
 // ==UserScript==
 // @name       KTXP&dmhyTorrentLinkToMagnet
 // @namespace  http://KTXP&dmhyTorrentLinkToMagnet/
-// @version    2.3
-// @description  在极影首页以及搜索页添加一个的绿底白箭头，链接为该资源的磁链；在动漫花园把蓝箭头的链接换成磁链。
+// @version    2.4
+// @description  在极影搜索页添加一个的绿底白箭头，链接为该资源的磁链。PS:总觉得这脚本快到寿终正寝的时候了
 // @match      http://bt.ktxp.com/search.php*
-// @match	   *://share.dmhy.org
-// @match	   *://share.dmhy.org/topics/list*
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @copyright  2014.01.17, JMNSY
 // ==/UserScript==
@@ -17,10 +15,10 @@ jQuery().ready(function(){
         link = jQuery(".quick-down");
         switchy = 1;
     }
-    if(jQuery(".download-arrow[title='種子']").length > 0){
-        link = jQuery(".download-arrow[title='種子']");
-        switchy = 0;
-    }
+//    if(jQuery(".download-arrow[title='種子']").length > 0){
+//        link = jQuery(".download-arrow[title='種子']");
+//        switchy = 0;
+//    }
     link.each(function(){
         temp = jQuery(this).clone();
         str = jQuery(this).attr("href");
@@ -30,7 +28,7 @@ jQuery().ready(function(){
         magnet = "magnet:?xt=urn:btih:" + hash;
         if(switchy == 1)
             jQuery(this).before(temp.attr("href",magnet));
-        else
-            jQuery(this).attr("href",magnet);
+//        else
+//            jQuery(this).attr("href",magnet);
     });
 });
